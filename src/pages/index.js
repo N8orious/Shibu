599c22b04fdf3b7e10f7e7a6eb4980531a137b45
N8orious <61679003+N8orious@users.noreply.github.com>
2020-11-28 23:34:12 +0530

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Text, Box } from "@quarkly/widgets";
+import { Theme, Link, Image } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
-import { Override, Menu, Section } from "@quarkly/components";
+import { Override, Section } from "@quarkly/components";
 export default (() => {
 	return <Theme theme={theme}>
 		<Helmet>
@@ -13,61 +13,15 @@ export default (() => {
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
 		<Section>
-			<Box
-				display="flex"
-				padding="12px 0"
-				justify-content="space-between"
-				align-items="center"
-				flex-direction="row"
-				md-flex-direction="column"
-			>
-				<Text margin="0" md-margin="0px 0 20px 0" text-align="left" font="--lead">
-					Shivani Pothirajan
-				</Text>
-				<Menu
-					display="flex"
-					justify-content="center"
-					font="--base"
-					font-weight="700"
-					md-flex-direction="column"
-					md-align-items="center"
-				>
-					<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
-					<Override slot="link-active" color="--primary" />
-					<Override slot="item" padding="6px" />
-				</Menu>
-			</Box>
-		</Section>
-		<Section
-			padding="60px 0"
-			sm-padding="40px 0"
-			min-height="600px"
-			sm-min-height="auto"
-			background="--color-light"
-			display="flex"
-			flex-direction="column"
-		>
-			<Override slot="SectionContent" flex="1 1 auto" />
-			<Text
-				font="--base"
-				text-transform="uppercase"
-				letter-spacing="1px"
-				max-width="850px"
-				color="--grey"
-				margin="auto 0px 0px 0px"
-			>
-				Eyebrow
-			</Text>
-			<Text
-				as="h1"
-				margin="8px 0px 32px"
-				font="--headline1"
-				md-font="--headline2"
-				color="--dark"
-				max-width="850px"
-			>
-				Fuck You
-			</Text>
+			<Override slot="SectionContent" sm-width="64px" sm-min-width="64px" />
+			<Image
+				width="64px"
+				height="64px"
+				src="https://uploads.quarkly.io/5fc28a552c4ef2001e32bd75/images/Drack_logo_black.png?v=2020-11-28T17:51:05.029Z"
+				position="static"
+				margin="0px 0px 0px 0"
+				sm-margin="0px 0px 0px 0px"
+			/>
 		</Section>
 		<Link
 			font={"--capture"}

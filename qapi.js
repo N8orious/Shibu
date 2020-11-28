0b57a24f12de2f324d44e03f64a1c5aa6216eea5
N8orious <61679003+N8orious@users.noreply.github.com>
2020-11-28 23:07:35 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc28a552c4ef2001e32bd78",
+				"5fc28a552c4ef2001e32bd7a"
+			]
+		},
+		"5fc28a552c4ef2001e32bd78": {
+			"id": "5fc28a552c4ef2001e32bd78",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc28a552c4ef2001e32bd7a": {
+			"id": "5fc28a552c4ef2001e32bd7a",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}

296671531740cfb380ba2e369221fe9fa329ed11
N8orious <61679003+N8orious@users.noreply.github.com>
2020-11-29 00:06:33 +0530

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,17 +12,26 @@ export default (() => {
 			<meta name={"description"} content={"Web site created using quarkly.io"} />
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
-		<Section>
-			<Override slot="SectionContent" sm-width="64px" sm-min-width="64px" />
+		<Section sm-padding="5px 0 10px 0" background="#000000">
+			<Override slot="SectionContent" sm-width="64px" sm-max-width="64px" sm-min-width="64px" />
 			<Image
 				width="64px"
 				height="64px"
-				src="https://uploads.quarkly.io/5fc28a552c4ef2001e32bd75/images/Drack_logo_black.png?v=2020-11-28T17:51:05.029Z"
+				src="https://uploads.quarkly.io/5fc28a552c4ef2001e32bd75/images/Drac_logo_brightred.png?v=2020-11-28T18:33:34.247Z"
 				position="static"
 				margin="0px 0px 0px 0"
 				sm-margin="0px 0px 0px 0px"
 			/>
 		</Section>
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			min-height="600px"
+			sm-min-height="auto"
+			background="--color-light"
+			display="flex"
+			flex-direction="column"
+		/>
 		<Link
 			font={"--capture"}
 			font-size={"10px"}
